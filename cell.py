cb764e30b5fe952724a5f17cd4f069ad82d472dc
fascto <fascto.05@gmail.com>
2025-05-18 03:18:20 -0300

    Añadiendo la generacion de celulas mediante el click

--- cell.py
+++ cell.py
@@ -1,3 +1,15 @@
 class cell:
-    def __init__(self):
+    def __init__(self, x_pos, y_pos):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.alive = 0
+
+
+    def check_neighbors():
+        pass
+
+    def dead():
+        pass
+
+    def born():
         pass
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame    
+import numpy as np
+import time
 
 # Colors
 
@@ -8,16 +10,20 @@ WHITE = (250, 250, 250)
 # Screen size variable
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGTH = 640
+SCREEN_HEIGTH = 800
 
+number_cell_x = 25
+number_cell_y = 25
 
-
+dim_cell_x = SCREEN_WIDTH // number_cell_x
+dim_cell_y = SCREEN_HEIGTH // number_cell_y
 
 def game_loop():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
     clock = pygame.time.Clock()
+    cell_state = np.zeros((number_cell_y, number_cell_x))
     running = True  
 
 
@@ -26,28 +32,47 @@ def game_loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_position = pygame.mouse.get_pos()
+                    draw_cell(screen, mouse_position, cell_state) 
 
-      
         screen.fill(BG)
-
-        # RENDER YOUR GAME HERE
-        
-        draw_grid(15, screen)
-
-        # flip() the display to put your work on screen
+        draw_grid(screen)
+        draw_active_cells(screen, cell_state) 
         pygame.display.flip()
+        clock.tick(60)
 
-        clock.tick(60)  # limits FPS to 60
+def draw_cell(screen, coords, cell_state):
+    
+    x = coords[0] // dim_cell_x
+    y = coords[1] // dim_cell_y
+    
+    if cell_state[y, x] == 0:
+        cell_state[y, x] = 1
+    else:
+        cell_state[y, x] = 0
+    
+
+def draw_active_cells(screen, cell_state):
+    for y in range(number_cell_y):
+        for x in range(number_cell_x):
+            if cell_state[y, x] == 1:
+                pygame.draw.rect(screen, WHITE, (x*dim_cell_x, y*dim_cell_y, dim_cell_x, dim_cell_y))
 
 
-def draw_grid(tile_size, screen):
-    for v in range(0, SCREEN_WIDTH, tile_size):
-        pygame.draw.line(screen, WHITE, (v, 0), (v, SCREEN_HEIGTH))
-    for h in range(0, SCREEN_HEIGTH, tile_size):
-        pygame.draw.line(screen, WHITE, (0, h), (SCREEN_WIDTH, h))
 
-def check_neighbors():
-    pass
+def draw_grid(screen): 
+    for y in range(0, number_cell_y):
+        for x in range(0, number_cell_x):  
+            cell_coords = [
+                (x * dim_cell_x, y * dim_cell_y),
+                ((x + 1) * dim_cell_x, y * dim_cell_y),
+                ((x + 1) * dim_cell_x, (y + 1) * dim_cell_y),
+                (x * dim_cell_x, (y + 1) * dim_cell_y)
+            ]
+            pygame.draw.polygon(screen, WHITE, cell_coords, 1) 
+
+
 
 if __name__ == "__main__":
 
