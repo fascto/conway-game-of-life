6e03976e0e67badf9f55446d3285580deb0fad0f
fascto <fascto.05@gmail.com>
2025-05-28 12:42:54 -0300

    Funcional

--- cell.py
+++ cell.py
@@ -1,14 +1,24 @@
-class cell:
-    def __init__(self, x_pos, y_pos):
+class Cell:
+    def __init__(self, x_pos, y_pos, status=1):
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.alive = 0
+        self.alive = status
 
 
-    def check_neighbors():
-        pass
+    def check_neighbors(self, cell_state_map) -> int:
 
-    def dead():
+        neighbors = 0
+
+        for x in range(self.x_pos-1, self.x_pos+1):
+            for y in range(self.y_pos-1, self.y_pos+1): 
+                if cell_state_map[x][y] == 1:
+                    neighbors+=1
+                else:
+                    continue
+        
+        return neighbors
+
+    def check_dead(self):
         pass
 
     def born():
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 import numpy as np
 import time
 
+import os
+os.environ['SDL_VIDEO_X11_FORCE_EGL'] = '1'
+
 # Colors
 
 BG = (30, 30, 30)
@@ -12,8 +15,8 @@ WHITE = (250, 250, 250)
 SCREEN_WIDTH = 800
 SCREEN_HEIGTH = 800
 
-number_cell_x = 25
-number_cell_y = 25
+number_cell_x = 50
+number_cell_y = 50
 
 dim_cell_x = SCREEN_WIDTH // number_cell_x
 dim_cell_y = SCREEN_HEIGTH // number_cell_y
@@ -25,32 +28,66 @@ def game_loop():
     clock = pygame.time.Clock()
     cell_state = np.zeros((number_cell_y, number_cell_x))
     running = True  
+    playing = False
 
+    # Funciones que se ejecutan una sola vez (de generacion)
 
     while running:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_position = pygame.mouse.get_pos()
-                    draw_cell(screen, mouse_position, cell_state) 
+            # elif event.type == pygame.MOUSEBUTTONDOWN:
+            #     mouse_position = pygame.mouse.get_pos()
+            #     draw_cell(screen, mouse_position, cell_state)
+
+            elif pygame.mouse.get_pressed()[0]:
+                mouse_position = pygame.mouse.get_pos()
+                draw_cell(screen, mouse_position, cell_state)
+
+            elif pygame.mouse.get_pressed()[2]:
+                mouse_position = pygame.mouse.get_pos()
+                delete_cell(screen, mouse_position, cell_state)
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if playing == True:
+                        playing = False
+                    else: 
+                        playing = True
+            
+                if event.key == pygame.K_BACKSPACE:
+                    cell_state = clear_grid()
+                    screen.fill(BG)
+                    draw_grid(screen)
+                    draw_active_cells(screen, cell_state)
+                    pygame.display.flip()
+
+
+        if playing:
+            cell_state = update_game(cell_state)
+            time.sleep(0.05)              
 
         screen.fill(BG)
         draw_grid(screen)
-        draw_active_cells(screen, cell_state) 
+        draw_active_cells(screen, cell_state)
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(300)
 
+# Me genera y dibueja la celula en la pantalla.
 def draw_cell(screen, coords, cell_state):
-    
+
     x = coords[0] // dim_cell_x
     y = coords[1] // dim_cell_y
     
-    if cell_state[y, x] == 0:
-        cell_state[y, x] = 1
-    else:
-        cell_state[y, x] = 0
+    cell_state[y, x] = 1 
+    
+
+def delete_cell(screen, coords, cell_state):
+    x = coords[0] // dim_cell_x
+    y = coords[1] // dim_cell_y
+
+    cell_state[y, x] = 0
     
 
 def draw_active_cells(screen, cell_state):
@@ -58,6 +95,49 @@ def draw_active_cells(screen, cell_state):
         for x in range(number_cell_x):
             if cell_state[y, x] == 1:
                 pygame.draw.rect(screen, WHITE, (x*dim_cell_x, y*dim_cell_y, dim_cell_x, dim_cell_y))
+                
+def clear_grid():
+    return np.zeros((number_cell_y, number_cell_x))
+
+def update_game(cell_state):
+    new_state = np.copy(cell_state)  
+    rows, cols = cell_state.shape
+    
+    for y in range(rows):
+        for x in range(cols):
+            neighbors = count_neighbors(cell_state, x, y)
+            
+            if cell_state[y, x] == 1:  
+                if neighbors < 2 or neighbors > 3:
+                    new_state[y, x] = 0
+                else: 
+                    print(f"La celula va a seguir viva")             
+            else:  
+                if neighbors == 3:
+                    new_state[y, x] = 1  
+                    print(f"La celula va a NACER")
+                    
+    
+    return new_state
+
+
+def count_neighbors(grid, x: int, y: int) -> int:
+
+    neighbor_cells = [
+        (x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
+        (x, y - 1),                   (x, y + 1),
+        (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)
+    ]
+    count = 0
+
+    for x,y in neighbor_cells:
+        if x >= 0 and y >= 0:
+            try:
+                count += grid[y, x]
+            except:
+                pass
+    return count
+
 
 
 
